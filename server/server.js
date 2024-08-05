724fe33e1a29f21f693bd6d546c3b9d2d971632b
Djebara Abdelali <jbaraali18@gmail.com>
2024-08-05 12:17:47 +0100

    feat(backend): setup backend project

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(cors());
+
+let items = [
+  {
+    id: 1,
+    title: "Item 1",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+  },
+];
+
+app.get("/api/items", (req, res) => {
+  res.json(items);
+});
+
+app.post("/api/items", (req, res) => {
+  const newItem = req.body;
+  items.push(newItem);
+  res.status(201).json(newItem);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
