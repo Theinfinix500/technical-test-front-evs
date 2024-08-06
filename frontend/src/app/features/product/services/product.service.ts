a60e671c7a4f1c3786ccfb9fa58b7fa781e076d7
Djebara Abdelali <jbaraali18@gmail.com>
2024-08-06 09:19:32 +0100

    feat(frontend): implement product service

---
+++ frontend/src/app/features/product/services/product.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Product } from '@core/models';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  http: HttpClient = inject(HttpClient);
+
+  loadProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>('http://localhost:3000/api/items');
+  }
+
+  saveProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>('http://localhost:3000/api/items', product);
+  }
+}
